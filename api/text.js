26b2bde46d7b7d1587639e5f66f5104ac3b93e5e
timothym4r <130308583+timothym4r@users.noreply.github.com>
2024-11-21 14:36:17 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/text.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send("Hi. I am pasarib1, and I have learned how to deploy a serverless app!");
+  }
